10e66fc878726443fd959926111c9ffbd67d91c3
Kevin <lopezkevin175@gmail.com>
2018-09-11 13:22:29 -0400

    cleaned up code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,53 @@
 class PostsController < ApplicationController
-  def index
-  end
+   def index
+      @posts = [
+         {
+            :title => "First Post",
+            :content => "this is the content area",
+            :slug => "first-post",
+            :image_url => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUuehBOhSoZ1-UDjO3J5jOndvGLYBy6hN4FtIX1dzv-Yt9HhUX",
+            :feature => false
+         },
+         {
+            :title => "Top 10 Javascript Frameworks",
+            :content => "this is the content area",
+            :slug => "top-10-javascript-frameworks",
+            :image_url => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUuehBOhSoZ1-UDjO3J5jOndvGLYBy6hN4FtIX1dzv-Yt9HhUX",
+            :feature => false        
+         },
+         {
+            :title => "Top 10 PHP Frameworks",
+            :content => "this is the content area",
+            :slug => "top-10-php-frameworks",
+            :image_url => "https://i.kym-cdn.com/photos/images/original/001/183/505/a22.jpg",
+            :feature => true        
+         },
+         {
+            :title => "Top 10 Node Frameworks",
+            :content => "this is the content area",
+            :slug => "top-10-node-frameworks",
+            :image_url => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUuehBOhSoZ1-UDjO3J5jOndvGLYBy6hN4FtIX1dzv-Yt9HhUX",
+            :feature => false        
+         }
+      ]
+   end
 
-  def show
-  end
+   def show
+   end
 
-  def new
-  end
+   def new
+   end
 
-  def create
-  end
+   def create
+   end
 
-  def edit
+   def edit
 
-  end
+   end
 
-  def update
-  end
+   def update
+   end
 
-  def destroy
-  end
+   def destroy
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/show'
-  get 'posts/new'
-  post'posts/create'
-  put 'posts/update'
-  delete 'posts/destroy'
-  root 'posts#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   get 'posts/show'
+   get 'posts/new'
+   post'posts/create'
+   put 'posts/update'
+   delete 'posts/destroy'
+   root 'posts#index'
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
